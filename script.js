91aac975674d7dbf5d62fbc195c4138337590430
Ray <qc@pop-os.localdomain>
2021-12-13 12:13:20 +0530

    gmaps added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const userLocation = document.querySelector('.location');
 const btn = document.querySelector('.btn');
+const gMaps = document.querySelector('.g-maps');
+
 
 btn.addEventListener('click', () => {
   navigator.geolocation.getCurrentPosition(getPosition);
@@ -11,5 +13,9 @@ function getPosition (position) {
     Latitude:${position.coords.latitude} ,
     Longitude:${position.coords.longitude}
     `
+    gMaps.href = `https://www.google.com/maps/@${position.coords.latitude},${position.coords.longitude},15z`
+    gMaps.innerHTML="Click here to Gmaps"
 }
+
+
 
